3b2c927e82a9206d47d2aa402528e817af9d8c05
hobbit04 <prlmejake9@gmail.com>
2024-06-21 09:44:16 +0900

    1874

--- 1874.py
+++ 1874.py
@@ -7,11 +7,22 @@ for i in range(n):
     arr.append(int(input()))
 
 i = 2  # for stack
-for target in arr:    
+for target in arr:
     while stack == [] or stack[-1] < target:
         stack.append(i)
         i += 1
         ans += "+\n"
+    
+    if stack == []:
+        stack.append(i)
+        i += 1
+        ans += "+\n"
+    elif stack[-1] < target:
+        dif = target - stack[-1]
+        i += dif
+        ans += "+\n" * dif
+        
+
     if stack[-1] == target:
         del stack[-1]
         ans += "-\n"
